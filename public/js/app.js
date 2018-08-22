5931e689081a3b71d29b21bbd16aae9f275bbf4d
TuoDi <337361454@qq.com>
2018-08-22 17:38:21 +0800

    10tijiao

--- public/js/app.js
+++ public/js/app.js
@@ -60380,7 +60380,7 @@ var api_url = '';
 
 switch ("development") {
     case 'development':
-        api_url = 'http://roast.test/api/v1';
+        api_url = 'http://120.79.20.43:8080/api/v1';
         break;
     case 'production':
         api_url = 'http://120.79.20.43:8080/api/v1';
