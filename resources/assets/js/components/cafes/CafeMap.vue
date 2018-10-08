0ba9bc22fa89784e3935a57621f4d3b3ce760d5d
TuoDi <337361454@qq.com>
2018-10-08 14:13:12 +0800

    10-1-6地图上标记

--- resources/assets/js/components/cafes/CafeMap.vue
+++ resources/assets/js/components/cafes/CafeMap.vue
@@ -34,13 +34,60 @@ export default {
         }
     },
     data(){
-        return {}
+        return {
+            markers: []
+        }
+    },
+    computed: {
+        cafes(){
+            return this.$store.getters.getCafes;
+        }
     },
     mounted() {
-        this.map = new AMap.Map('cafe-map', {
+        this.map = new AMap.Map('cafe-map', {//默认地图
             center: [this.latitude, this.longitude],
             zoom: this.zoom
         });
+        // 清除并重构点标记
+        this.clearMarkers();
+        this.buildMarkers();
+    },
+    methods: {
+        // 为所有咖啡店创建点标记
+        buildMarkers() {
+            // 清空点标记数组
+            this.markers = [];
+
+            // 遍历所有咖啡店并为每个咖啡店创建点标记
+            for (var i = 0; i < this.cafes.length; i++) {
+
+                // 通过高德地图 API 为每个咖啡店创建点标记并设置经纬度
+                var marker = new AMap.Marker({
+                    position: AMap.LngLat(parseFloat(this.cafes[i].latitude), parseFloat(this.cafes[i].longitude)),
+                    title: this.cafes[i].name
+                });
+
+                // 将每个点标记放到点标记数组中
+                this.markers.push(marker);
+            }
+
+            // 将所有点标记显示到地图上
+            this.map.add(this.markers);
+        },
+        // 从地图上清理点标记
+        clearMarkers() {
+            // 遍历所有点标记并将其设置为 null 从而从地图上将其清除
+            for (var i = 0; i < this.markers.length; i++) {
+                this.markers[i].setMap(null);
+            }
+        },
+    },
+    watch: {
+        // 一旦 cafes 有更新立即重构地图点标记
+        cafes () {
+            this.clearMarkers();
+            this.buildMarkers();
+        }
     }
 }
 </script>
