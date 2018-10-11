d7b0e0f7a38056208db066d489cc72b905f53f7e
TuoDi <337361454@qq.com>
2018-10-11 11:54:28 +0800

    10-4-2loader测试

--- public/js/app.js
+++ public/js/app.js
@@ -59778,7 +59778,7 @@ var render = function() {
           )
         : _vm._e(),
       _vm._v(" "),
-      _c("Loader", {
+      _c("loader", {
         directives: [
           {
             name: "show",
