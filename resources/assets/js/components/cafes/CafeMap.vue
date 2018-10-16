6c722a91fced1b514ea45cf2bca065678301de9d
TuoDi <337361454@qq.com>
2018-10-16 16:28:27 +0800

    10-7-1自定义消息窗体

--- resources/assets/js/components/cafes/CafeMap.vue
+++ resources/assets/js/components/cafes/CafeMap.vue
@@ -71,19 +71,32 @@
                         position: new AMap.LngLat(parseFloat(this.cafes[i].latitude), parseFloat(this.cafes[i].longitude)),
                         title: this.cafes[i].name + this.cafes[i].location_name,
                         icon: icon,
-                        map: this.map
+                        extData: {
+                            'cafe': this.cafes[i]
+                        }
                     });
                     //给标记添加信息
                     marker.setLabel({
                         offset: new AMap.Pixel(-5, -20),
                         content: this.cafes[i].name + '---' + this.cafes[i].location_name
                     });
-
                     // 自定义信息窗体
+                    var contentString = '<div class="cafe-info-window">' +
+                        '<div class="cafe-name">' + this.cafes[i].name + this.cafes[i].location_name + '</div>' +
+                        '<div class="cafe-address">' +
+                        '<span class="street">' + this.cafes[i].address + '</span>' +
+                        '<span class="city">' + this.cafes[i].city + '</span> ' +
+                        '<span class="state">' + this.cafes[i].state + '</span>' + +
+                        '<a href="/#/cafes/' + this.cafes[i].id + '">Visit</a>' +
+                        '</div>' +
+                        '</div>';
+                    marker.content = contentString;
+
+                    /* // 自定义信息窗体
                     var infoWindow = new AMap.InfoWindow({
                         content: '地址：' + this.cafes[i].state + this.cafes[i].city + this.cafes[i].address + "<br/>" + 
                                  '电话：110'
-                    });
+                    }); */
                     this.infoWindows.push(infoWindow);
                     // 绑定点击事件到点标记对象，点击打开上面创建的信息窗体
                     marker.on('click', function () {
