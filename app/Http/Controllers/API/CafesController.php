5c0f135df0f15b6a0e81fd6ac70873e1970fae18
TuoDi <337361454@qq.com>
2018-10-22 17:04:46 +0800

    10-11-10调试

--- app/Http/Controllers/API/CafesController.php
+++ app/Http/Controllers/API/CafesController.php
@@ -36,7 +36,7 @@ class CafesController extends Controller
     }
 
     public function postNewCafe(/* StoreCafe */Request $request){
-        dd($request->toArray());
+        dd($request->all());
         // 已添加的咖啡店
         $addedCafes = [];
         // 所有位置信息
