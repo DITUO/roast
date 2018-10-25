74c480335efb34e000594b9923262533ab5e0daa
TuoDi <337361454@qq.com>
2018-10-25 09:24:59 +0800

    10-14-3tiaoshi

--- app/Http/Controllers/API/CafesController.php
+++ app/Http/Controllers/API/CafesController.php
@@ -15,7 +15,7 @@ use App\Utilities\Tagger;
 use Carbon\Carbon;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
+use Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\File;
 
@@ -35,7 +35,8 @@ class CafesController extends Controller
     }
 
     public function getCafe($id){
-        \Log::info(Auth::user());
+        $user = Auth::user();
+        \Log::info($user);
         $cafe = Cafe::where('id',$id)
                 ->with('brewMethods')
                 ->withCount('userLike')
