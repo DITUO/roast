2f7b8da76b4392dd05be5074c12bc24f40bc1002
DITUO <337361454@qq.com>
2018-10-30 15:06:31 +0800

    10.30-23

--- app/Services/CafeService.php
+++ app/Services/CafeService.php
@@ -69,7 +69,6 @@ class CafeService
         // 如果选择已有的公司，则更新公司信息，否则新增
         if (isset($data['company_id'])) {
             $company = Company::where('id', '=', $data['company_id'])->first();
-            dd($company);
             if (isset($data['company_name'])) {
                 $company->name = $data['company_name'];
             }
